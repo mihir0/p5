81352aec45f8fd6bd14b0b8c954e6ea513a73161
mihirparshy <mihir.parshy@hotmail.com>
2018-11-26 20:55:27 -0500

    brushes and containers

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 window.onload = start;
 var chart1, chart2, chart3, chart4;
 function start() {
-    var width = 700, height = 400, radius = 2;
+    var width = 700, height = 500, radius = 2;
     var padding = {left: 80, right: 80, top: 80, bottom: 80};
 
     //Method to move data marks to the front
     //Source: https://gist.github.com/trtg/3922684 
-    d3.selection.prototype.moveToFront = function() {
-        return this.each(function(){
-        this.parentNode.appendChild(this);
-        });
-    };
+    // d3.selection.prototype.moveToFront = function() {
+    //     return this.each(function(){
+    //     this.parentNode.appendChild(this);
+    //     });
+    // };
     
     chart1 = d3.select('#chart1')
         .append('svg')
@@ -92,6 +92,36 @@ function start() {
             .tickSize(1)
             .tickFormat(function(d){ return String(d / 1000) + 'k'; })
             .orient("left");
+
+        // CREATE BRUSHES & CONTAINERS
+        var brush1 = d3.svg.brush().extent([debtExtent[0], costExtent[0]], [debtExtent[1], costExtent[1]])
+            .x(debtScale).y(costScale)
+            .on("brush", function(){})
+            .on("brushstart", function(){ console.log("starting brush"); });
+        var brush1_container = chart1.append('g')
+            .attr("class", "brush")
+            .call(brush1)
+        var brush2 = d3.svg.brush().extent([salaryExtent[0], admissionExtent[0]], [salaryExtent[1], admissionExtent[1]])
+            .x(salaryScale).y(admissionScale)
+            .on("brush", function(){})
+            .on("brushstart", function(){ console.log("starting brush"); });
+        var brush2_container = chart2.append('g')
+            .attr("class", "brush")
+            .call(brush2)
+        var brush3 = d3.svg.brush().extent([debtExtent[0], incomeExtent[0]], [debtExtent[1], incomeExtent[1]])
+            .x(debtScale).y(incomeScale)
+            .on("brush", function(){})
+            .on("brushstart", function(){ console.log("starting brush"); });
+        var brush3_container = chart3.append('g')
+            .attr("class", "brush")
+            .call(brush3)
+        var brush4 = d3.svg.brush().extent([salaryExtent[0], expenditureExtent[0]], [salaryExtent[1], expenditureExtent[1]])
+            .x(salaryScale).y(expenditureScale)
+            .on("brush", function(){})
+            .on("brushstart", function(){ console.log("starting brush"); });
+        var brush4_container = chart4.append('g')
+            .attr("class", "brush")
+            .call(brush4)
 
         // DRAW AXES
         chart1
@@ -238,14 +268,10 @@ function start() {
 function onClick(d) {
     // highlight clicked datapoint on all graphs
     console.log(d);
-    chart1.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index))
-        .moveToFront();
-    chart2.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index))
-        .moveToFront();
-    chart3.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index))
-        .moveToFront();
-    chart4.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index))
-        .moveToFront();
+    chart1.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index));
+    chart2.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index));
+    chart3.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index));
+    chart4.selectAll('circle').classed('clicked', (circle) => (circle.index === d.index));
 }
 
 function mouseover() {
